20e641d969f1d4ca3aedd6c93eef94e9e14272d6
PacDraft <Kirsten@PacificDraftWorks.com>
2021-11-05 14:29:36 -0700

    added file for js & started code

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+var todaysDate = moment().format('dddd, MMMM do YYYY');
+$("#currentDay").html(todaysDate);
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function() {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem (time, text);
+    })
+
+    function trackTime() {
+        var currentTime = moment().hour();
+
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attri("id").split("hour")[1]);
+
+            if (blockTime < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeCLass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeCLass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+trackTime();
+
+})
