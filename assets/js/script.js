a53a12568b28adc594b903084d297fa647d08a20
PacDraft <Kirsten@PacificDraftWorks.com>
2021-11-05 16:59:55 -0700

    added comments to describe functions & variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,60 @@
-var todaysDate = moment().format('dddd, MMMM do YYYY');
+// Variable for current date
+var todaysDate = moment().format('dddd, MMM Do YYYY');
+// Display current date using JQuery
 $("#currentDay").html(todaysDate);
 
 $(document).ready(function () {
-    $(".saveBtn").on("click", function() {
+    // click listener for save button
+    $(".saveBtn").on("click", function () {
+        // Pull nearby values
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        localStorage.setItem (time, text);
+        // Save text entered into time blocks in local storage
+        localStorage.setItem(time, text);
     })
+   
+// Function to track the timeblocks
+    function timeTracker() {
+        // current hour
+        var timeNow = moment().hour();
 
-    function trackTime() {
-        var currentTime = moment().hour();
-
+        // loop over timeblocks
         $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attri("id").split("hour")[1]);
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-            if (blockTime < currentTime) {
+            // check the current time and add background colors
+            if (blockTime < timeNow) {
                 $(this).removeClass("future");
-                $(this).removeCLass("present");
+                $(this).removeClass("present");
                 $(this).addClass("past");
             }
-            else if (blockTime === currentTime) {
+            else if (blockTime === timeNow) {
                 $(this).removeClass("past");
-                $(this).removeCLass("future");
+                $(this).removeClass("future");
                 $(this).addClass("present");
             }
             else {
                 $(this).removeClass("present");
                 $(this).removeClass("past");
                 $(this).addClass("future");
+
             }
         })
     }
 
+    // Pull data from local storage if any has been input
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-$("#hour8 .description").val(localStorage.getItem("hour8"));
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
-
-trackTime();
-
+    // Call function to operate timeblocks
+    timeTracker();
 })
